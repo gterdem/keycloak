2d18307e1f2847b3f17b4cd2b90768aae0db6f98
Galip Tolga Erdem <galiperdem@gmail.com>
2023-07-14 12:32:54 -0400

    Initial commit

---
+++ Gte/Abp/Keycloak/AbpKeycloakClientOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gte.Abp.Keycloak;
+
+public class AbpKeycloakClientOptions
+{
+    /// <summary>
+    /// The URL of the keycloak server. The default is "http://localhost:8080/"
+    /// </summary>
+    public Uri Url { get; set; }
+    /// <summary>
+    /// The name of the realm that the keycloak client connects to.
+    /// </summary>
+    public string? RealmName { get; set; }
+
+    public AbpKeycloakClientOptions()
+    {
+        Url = new Uri("http://localhost:8080");
+        RealmName = "master";
+    }
+}
---
+++ Gte/Abp/Keycloak/AbpKeycloakModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Gte.Abp.Keycloak;
+
+public class AbpKeycloakModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+
+        Configure<AbpKeycloakClientOptions>(options =>
+            {
+                options.Url = new Uri(configuration["Keycloak:url"]!);
+                options.RealmName = configuration["Keycloak:realmName"];
+            }
+        );
+    }
+}
